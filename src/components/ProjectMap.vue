f4f55bcd2a8054ad947ba8bf58571850c2232b38
Thomas Tartiere <thomas.tartiere@gmail.com>
2019-04-06 20:45:23 -0700

    feat: Build custom GoogleMap component

--- src/components/ProjectMap.vue
+++ src/components/ProjectMap.vue
@@ -1,33 +1,71 @@
 <template>
-  <GmapMap :center="{lat:30, lng:-10}" :zoom="3" map-type-id="terrain" :options="mapOptions"></GmapMap>
+  <div class="google-map" id="googleMap"></div>
 </template>
 
 <script>
 export default {
-  name: "ProjectMap",
-  data() {
+  name: "project-map",
+  data: function() {
     return {
-      mapOptions: {
-        zoomControl: true,
+      options: {
+        zoom: 3,
+        center: new google.maps.LatLng(30, -10),
         mapTypeControl: true,
-        MapTypeControlOptions: {
-          position: "BOTTOM_CENTER"
-        },
-        fullScreenControl: true,
-        scaleControl: false,
-        streetViewControl: true
-      }
+        mapTypeControlOptions: {
+          position: google.maps.ControlPosition.BOTTOM_CENTER
+        }
+      },
+      projects: [],
+      map: null,
+      markers: []
     };
+  },
+  methods: {
+    // load the projects
+    loadProjects() {
+      this.projects = [
+        { name: "P1", latitude: 30, longitude: -10, application: "geothermal" },
+        { name: "P2", latitude: 40, longitude: -10, application: "biomass" },
+        {
+          name: "P3",
+          latitude: -20,
+          longitude: -10,
+          application: "heat_recovery"
+        },
+        { name: "P4", latitude: -10, longitude: -10, application: "solar" }
+      ];
+    },
+    displayProjects() {
+      this.projects.forEach(project => {
+        const position = new google.maps.LatLng(
+          project.latitude,
+          project.longitude
+        );
+        const marker = new google.maps.Marker({
+          position,
+          map: this.map,
+          title: project.name
+        });
+        this.markers.push(marker);
+      });
+    }
+  },
+  mounted: function() {
+    this.loadProjects();
+    const element = document.getElementById("googleMap");
+    this.map = new google.maps.Map(element, this.options);
+    this.displayProjects(this.map);
   }
 };
 </script>
 
 <style scoped>
-.vue-map-container {
+.google-map {
   position: absolute;
   width: 100%;
+  top: 76px;
   bottom: 0;
-  top: 60px;
+  margin: 0 auto;
+  background: gray;
 }
 </style>
-
